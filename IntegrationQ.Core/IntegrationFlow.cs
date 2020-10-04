721b050925b7b9f096d1dcca47804bd5fb34e71e
adankin <dankin.anton.s@gmail.com>
2020-10-05 00:56:53 +0300

    solution created

---
+++ IntegrationQ.Core/IntegrationFlow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationQ.Core
+{
+    public class IntegrationFlow
+    {
+        public IntegrationFlow(IntegrationFlowOptions options)
+        {
+            
+        }
+    }
+}
---
+++ IntegrationQ.Distributor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationQ.Distributor
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+#if LOCALDEV
+            var service = new Service1();
+            service.DebugStartupAndStop(args);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
